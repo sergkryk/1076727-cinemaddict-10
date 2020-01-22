6a4b3982502d1de546bcffb912f07dbbe8a84b6a
sergkryk <41703448+sergkryk@users.noreply.github.com>
2020-01-22 21:09:10 +0300

    Добавляю генерацию имени профиля

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,20 @@
 import AbstractComponent from './abstract-component';
 
+const getProfileTitle = (moviesNumber) => {
+  let title = `Newbie`
+  if (moviesNumber >= 10 && moviesNumber <= 20) {
+    title = `Amateur`
+  }
+  if (moviesNumber > 20 && moviesNumber <= 30) {
+    title = `Expert`
+  }
+  if (moviesNumber > 30) {
+    title = `Guru`
+  }
+  return title;
+};
+
+
 export default class ProfileComponent extends AbstractComponent {
   constructor() {
     super();
@@ -7,7 +22,7 @@ export default class ProfileComponent extends AbstractComponent {
 
   getTemplate() {
     return `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
+    <p class="profile__rating">Movie ${getProfileTitle(55)}</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>
     `;
