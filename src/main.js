b38a659865cdc51abb7203e9865b0854db0bd94f
sergkryk <41703448+sergkryk@users.noreply.github.com>
2020-01-23 12:20:44 +0300

    Добавляю обработчики показа детальной карточки

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import StatsComponent from './components/stats';
 import SortComponent from './components/sort';
 import FilmsSectionComponent from './components/films-section';
 import FilmsListComponent from './components/films-list';
-import FilmCardComponent from './components/film-card';
+import CardComponent from './components/card';
+import CardDetailedComponent from './components/card-detailed';
 import ShowMoreButtonComponent from './components/show-more-button';
 import FilmsExtraComponent from './components/films-extra';
 
-import {renderElement} from './utils/render';
+import {renderElement, removeElement} from './utils/render';
 import {CARDS_NUMBER, ExtraHeadings} from './const';
 
 import {generateFilters} from './mock/filter';
 import {returnMovieCards} from './mock/movie-card';
+// import CardDetailedComponent from './components/card-detailed';
 
 const CARDS_NUMBER_BY_START = 5;
 const CARDS_NUMBER_BY_CLICK_MORE = 5;
@@ -48,10 +50,47 @@ const filmCardContainer = filmsListComponent.getElement().querySelector(`.films-
 
 let cardsToShow = CARDS_NUMBER_BY_START;
 
-movieCards.slice(0, cardsToShow).map((element) => renderElement(filmCardContainer, new FilmCardComponent(element)));
+const renderMovieCards = (cardsArray) => {
+  cardsArray.forEach((cardElement) => {
+    const card = new CardComponent(cardElement);
+    const cardDetailed = new CardDetailedComponent();
+
+    const onEscPressRemove = (evt) => {
+      if (evt.keyCode === 27) {
+        removeElement(cardDetailed);
+        document.removeEventListener(`keydown`, onEscPressRemove);
+      }
+    };
+
+    card.getElement().addEventListener(`click`, (evt) => {
+      if (evt.target.tagName === `IMG` || evt.target.tagName === `A` || evt.target.tagName === `H3`) {
+        renderElement(filmCardContainer, cardDetailed);
+        cardDetailed.getElement().querySelector(`.film-details__close-btn`).addEventListener(`click`, () => {
+          removeElement(cardDetailed);
+        });
+        document.addEventListener(`keydown`, onEscPressRemove);
+      }
+    });
+
+    renderElement(filmCardContainer, card);
+  });
+};
+
+renderMovieCards(movieCards.slice(0, cardsToShow));
 
 const showMoreButtonComponent = new ShowMoreButtonComponent();
 renderElement(filmsListComponent.getElement(), showMoreButtonComponent);
+
+showMoreButtonComponent.getElement().addEventListener(`click`, () => {
+  const prevCardsNumber = cardsToShow;
+  cardsToShow = cardsToShow + CARDS_NUMBER_BY_CLICK_MORE;
+
+  renderMovieCards(movieCards.slice(prevCardsNumber, cardsToShow));
+
+  if (cardsToShow >= movieCards.length) {
+    showMoreButtonComponent.getElement().remove();
+  }
+});
 
 const filmsSection = filmsSectionComponent.getElement();
 
@@ -65,28 +104,15 @@ movieCards
   .slice()
   .sort((a, b) => b.rating - a.rating)
   .slice(0, 2)
-  .map((element) => renderElement(filmsExtraRateComponent.getElement().querySelector(`.films-list__container`), new FilmCardComponent(element)))
+  .map((element) => renderElement(filmsExtraRateComponent.getElement().querySelector(`.films-list__container`), new CardComponent(element)))
 ;
 
 movieCards
   .slice()
   .sort((a, b) => b.comments.length - a.comments.length)
   .slice(0, 2)
-  .map((element) => renderElement(filmsExtraCommentComponent.getElement().querySelector(`.films-list__container`), new FilmCardComponent(element)))
+  .map((element) => renderElement(filmsExtraCommentComponent.getElement().querySelector(`.films-list__container`), new CardComponent(element)))
 ;
-
-const loadMoreButton = document.querySelector(`.films-list__show-more`);
-loadMoreButton.addEventListener(`click`, () => {
-  const prevCardsNumber = cardsToShow;
-  cardsToShow = cardsToShow + CARDS_NUMBER_BY_CLICK_MORE;
-
-  movieCards.slice(prevCardsNumber, cardsToShow)
-     .forEach((card) => renderElement(filmCardContainer, new FilmCardComponent(card), `beforeend`));
-
-  if (cardsToShow >= movieCards.length) {
-    loadMoreButton.remove();
-  }
-});
 
 const footerCounter = document.querySelector(`.footer__statistics`);
 footerCounter.innerHTML = `<p>${movieCards.length} movies inside</p>`;
